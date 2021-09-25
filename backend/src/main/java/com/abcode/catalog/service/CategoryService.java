006e30f683c52d627811d17f53667e1440864c4d
Arthur Barbosa <arthurbruno03@gmail.com>
2021-09-25 14:50:10 -0500

    created interface  CategoryRepository and CategoryService

---
+++ backend/src/main/java/com/abcode/catalog/service/CategoryService.java
@@ -0,0 +1,16 @@
+package com.abcode.catalog.service;
+
+import com.abcode.catalog.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+}
